b1a04df0d6ad4c159e9439642013bceebd0f5945
calvinjchan1 <calvinjchan1@gmail.com>
2019-03-06 07:38:00 -0700

    Created a temporary directory for images to go in

--- Python_Files/webcam.py
+++ Python_Files/webcam.py
@@ -1,8 +1,14 @@
 import cmdTest
 import cv2
+import os
+
+try:
+    os.mkdir("runtime")
+except:
+    print("Failed to create runtime directory")
 
 camera = cv2.VideoCapture(0)
 success, image = camera.read()
 if success:
     cv2.imshow("Test", image)
-    cv2.imwrite("test.jpg", image)
+    cv2.imwrite("runtime/test.jpg", image)
