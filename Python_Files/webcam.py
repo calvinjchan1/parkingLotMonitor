e5bb06439bdbc665cb4e9e0482b90af0f5b2a21d
calvinjchan1 <calvinjchan1@gmail.com>
2019-03-06 08:24:15 -0700

    Tested average time per picture

--- Python_Files/webcam.py
+++ Python_Files/webcam.py
@@ -13,12 +13,30 @@ except:
 #Take a picture and save it to the runtime directoy
 camera = cv2.VideoCapture(0)
 time.sleep(1); #Let windows get camera ready
-success, image = camera.read() #Take a screenshot
-if success:
-    cv2.imshow("Test", image)
-    cv2.imwrite("runtime/test.jpg", image)
-else:
-    print("Picture failed")
 
-time.sleep(1);
-print(cmdTest.scanPlate("runtime/test.jpg"))
+def takePicture():
+    '''
+    Takes a picture, scans for license plates, and
+    returns how long it took to do sos
+    '''
+    start = time.time()
+    success, image = camera.read() #Take a screenshot
+    if success:
+        #cv2.imshow("Test", image)
+        cv2.imwrite("runtime/test.jpg", image)
+    else:
+        print("Picture failed")
+    print(cmdTest.scanPlate("runtime/test.jpg"))
+    timeTaken = time.time()-start
+    print("Time Taken: " + str(timeTaken))
+    return timeTaken
+
+#Print out average time it takes to take each picture
+avgTime = 0
+total = 0
+count = 0
+while True:
+    total += takePicture()
+    count +=1
+    avgTime = total/count
+    print("Average time: " + str(avgTime))
