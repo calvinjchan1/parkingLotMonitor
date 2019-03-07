9d89d377062fe362daaf6f8ed1a7c756edc81488
calvinjchan1 <calvinjchan1@gmail.com>
2019-03-07 13:44:54 -0700

    added features to read and write parent bools to firebase

--- Python_Files/firebase.py
+++ Python_Files/firebase.py
@@ -1,10 +1,10 @@
-import pyrebase
+import pyrebase, getpass
 
 #https://github.com/thisbejim/Pyrebase for documentaion on pyrebase
 
 #Get login info
 email = input("Enter your email address: ")
-password = input("Enter your password: ")
+password = getpass.getpass("Enter your password: ")
 
 #Setup pyrebase
 config = {
@@ -19,9 +19,38 @@ user = auth.sign_in_with_email_and_password(email, password)
 print(user)
 db = firebase.database()
 
+#Example Code
+'''
 #Send Data
 db.child("parents").update({"test":True},user['idToken'])
 
 #Get data
 parents = db.child("parents").get(user['idToken'])
 print(parents.val())
+'''
+
+def getParent(parent):
+    '''
+    Returns a bool of whether the given parent is in the parking lot,
+    None if the parent doesn't exist
+    '''
+    data = db.child("parents").child(parent).get(user['idToken'])
+    inParkingLot = data.val()
+    if inParkingLot != None: #Make sure the parent is someone who exists
+        return inParkingLot
+    else:
+        return None
+
+def setParent(parent, value):
+    '''
+    Sets the given parent to the given value. Returns true if successful,
+    false if the parent doesn't exist
+    '''
+    if getParent(parent) != None: #Make sure parent exists
+        db.child("parents").update({parent: value}, user['idToken'])
+        return True
+    else:
+        return False
+
+setParent("Patrick", True)
+print(getParent("Patrick"))
