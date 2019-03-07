0750e2296e959e645ac50acbfafdb3438f360ed8
calvinjchan1 <calvinjchan1@gmail.com>
2019-03-07 12:53:35 -0700

    Retreived data from firebase

--- Python_Files/firebase.py
+++ Python_Files/firebase.py
@@ -5,19 +5,23 @@ import pyrebase
 #Get login info
 email = input("Enter your email address: ")
 password = input("Enter your password: ")
-APIKey = input("Enter the API Key: ")
 
 #Setup pyrebase
 config = {
-  "apiKey": APIKey,
+  "apiKey": "AIzaSyBzraFT2d7I2XAfkpinjeAb-BOZB-kHetY",
   "authDomain": "parkinglotmonitor.firebaseapp.com",
   "databaseURL": "https://parkinglotmonitor.firebaseio.com",
   "storageBucket": "parkinglotmonitor.appspot.com"
 }
-
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 user = auth.sign_in_with_email_and_password(email, password)
 print(user)
 db = firebase.database()
-db.child("parents").update({"test":False},user['idToken'])
+
+#Send Data
+db.child("parents").update({"test":True},user['idToken'])
+
+#Get data
+parents = db.child("parents").get(user['idToken'])
+print(parents.val())
