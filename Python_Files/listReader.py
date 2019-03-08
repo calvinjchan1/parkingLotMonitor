cbeea5fe3ad1a89406fdc49d9482ddbdf29bf4fc
calvinjchan1 <calvinjchan1@gmail.com>
2019-03-08 10:30:01 -0700

    Added getParent function in listReader

--- Python_Files/listReader.py
+++ Python_Files/listReader.py
@@ -3,4 +3,13 @@ import json
 with open('../secrets/test.json') as f:
     data = json.load(f)
 
-print(data)
+def getParent(plate):
+    '''
+    Tries to match the license plate to a list of known license plates
+    Returns the parent maching the license plate, false if the parent
+    wasn't found
+    '''
+    try:
+        return data[plate]
+    except KeyError:
+        return False
